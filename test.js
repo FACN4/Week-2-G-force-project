be714ea1fce4d48555419bad21da9f0ca0a6a838
Ben Newman <benjamin-newman@live.co.uk>
2018-07-10 23:39:48 +0300

    did addTodo and deleteTodo > #5

--- test.js
+++ test.js
@@ -1,25 +1,29 @@
 var test = require("tape");
 var logic = require("./logic");
 
-// test("Example test", function(t) {
-//   t.pass();
-//   t.end();
-// });
-test("addTodo returns an array", function(t) {
-  t.equal(logic.addTodo([1], [1]).isarray, [1, 1].isarray, "should return an array");
-  t.end();
-});
 
-test("addToDo returns correct values", function(t) {
-  t.same(logic.addTodo([1],[1]), [1,1], "should return [1, 1]");
-  console.log(logic.addTodo([1],[1]));
+test("addToDo", function(t) {
+  t.equal(logic.addTodo([1], [1]).isarray, [1, 1].isarray, "should return an array");
+
+  var actual = logic.addTodo([{id: 1,description: "grow a tree", done: false}], {description: "eat an apple"});
+  var expected = [{id: 1,description: "grow a tree", done: false}, {id: 2,description: "eat an apple", done: false}];
+  t.same(actual, expected, 'should return [{id: 1, description: "grow a tree", done :false}, {id: 2, description: "eat an apple", done: false}]');
   t.end();
 })
 
-test("addToDo returns correct values", function(t) {
-  var actual = logic.addTodo([{id : 1,task : "grow a tree", completed :false}], [{id : 2,task : "eat an apple", completed :false}])
-  var expected = [{id : 1,task : "grow a tree", completed :false}, {id : 2,task : "eat an apple", completed :false}]
-  t.same(actual, expected, 'should return [{id : 1,task : "grow a tree", completed :false}, {id : 2,task : "eat an apple", completed :false}]');
+
+test("deleteTodo", function(t) {
+  var actual = logic.deleteTodo([{id: 1, description: "grow a tree", done: false}], 1);
   console.log(actual);
+  var expected = [];
+  t.same(actual, expected, 'should return an empty array');
+  t.end();
+})
+
+test("markTodo", function(t) {
+  var actual = logic.markTodo([{id: 1, description: "grow a tree", done: false}], 1);
+  console.log(actual);
+  var expected = [{id: 1, description: "grow a tree", done: true}];
+  t.same(actual, expected, 'should be marked as done');
   t.end();
 })
